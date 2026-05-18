e08581293df40e963bb2fb85bb5dcaaa74eb3374
BoehmPa <pascalboehm2805@gmail.com>
2026-05-18 16:05:34 +0200

    test minimal timer function

--- function_app.py
+++ function_app.py
@@ -1,24 +1,13 @@
 import logging
 import azure.functions as func
 
-from collector import init_db, collect_once
-
 app = func.FunctionApp()
 
-
 @app.timer_trigger(
-    schedule="0 0 */6 * * *",
+    schedule="0 */5 * * * *",
     arg_name="timer",
     run_on_startup=True,
-    use_monitor=True,
+    use_monitor=True
 )
 def weather_collector(timer: func.TimerRequest) -> None:
-    logging.info("Weather collector function started.")
-
-    if timer.past_due:
-        logging.warning("Timer is past due.")
-
-    init_db()
-    collect_once()
-
-    logging.info("Weather collector function finished.")
+    logging.info("Minimal timer function loaded.")
